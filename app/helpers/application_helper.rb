1137af47483a229d13db206ef0374fdba80fe6fc
MayuFujimoto <mayufujimoto41@gmail.com>
2025-06-02 18:40:40 +0900

    X-card設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: "世代別言葉クイズ",
+      title: "世代別言葉クイズ！",
+      charset: "utf-8",
+      description: "世代別で流行った言葉のクイズです",
+      keywords: "クイズ, 世代, 言葉, 懐かしい",
+      canonical: request.original_url,
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: "website",
+        url: request.original_url,
+        image: "#{root_url}home.png",
+        locale: "ja_JP"
+      },
+      twitter: {
+        card: "summary_large_image",
+        image: "#{root_url}home.png"
+      }
+    }
+  end
 end
