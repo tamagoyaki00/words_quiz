e098d2e5c93a7b27ebec04d6a50ab7b55d277c6a
MayuFujimoto <mayufujimoto41@gmail.com>
2025-06-02 13:56:00 +0900

    全問正解者ページの修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -33,16 +33,21 @@ class CategoriesController < ApplicationController
       return
     end
 
-    # 解答の正誤判定
+    #解答の正誤判定
     selected_choice = Choice.find(selected_choice_id)
     increment_correct_count if selected_choice.correct_answer?
 
     session[:current_index] += 1
-    # 規定の問題数までループ
+    
+    #規定の問題数までループ,終わったら結果画面に遷移
     if session[:current_index] < session[:question_ids].size
       redirect_to category_path(id: session[:category_id])
+    elsif session[:correct_count] == session[:question_ids].size
+      Rails.logger.debug "パーフェクト確認"
+      Rails.logger.debug "session[:category_id] = #{session[:category_id].inspect}"
+      redirect_to quiz_perfect_path(category_id: session[:category_id])
     else
-      redirect_to quiz_result_path
+      redirect_to quiz_result_path(category_id: session[:category_id])
     end
   end
 
